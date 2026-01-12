8cef21138f2aedbf0be4629b119e229897d21053
HamplusTech <51116379+HamplusTech@users.noreply.github.com>
2026-01-12 19:56:42 +0100

    Add index.php with redirect to nested_flatten.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="5; url=./nested_flatten.php">
+    <title>Hamplus Technologies International - Hamplus Hub</title>
+</head>
+<body>
+    <h2>This page will redirect to another page in 5 seconds</h2>
+</body>
+</html>
